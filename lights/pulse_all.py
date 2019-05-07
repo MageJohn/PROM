6d1862e78aa7b75eb33a3adbde9f76abf5097fae
sch556 <sch556@york.ac.uk>
2019-05-07 15:08:02 +0100

    Create pulse_all.py

---
+++ lights/pulse_all.py
@@ -0,0 +1,14 @@
+import time
+from PyGlow import PyGlow
+from .. import constants
+
+b = constants.PIGLOW_B
+s = constants.PIGLOW_S
+pyglow = PyGlow(brightness = b, speed = s, pulse=True)
+
+pyglow.all(0) #Set all LEDs to 0
+
+pyglow.all(brightness=b)
+time.sleep(s/1000)
+
+pyglow.all(0) #Set all LEDs to 0
