202bc90f51b80f42db8de550fab7e97137709aec
MageJohn <magejohnyjtp@gmail.com>
2019-05-10 01:43:57 +0100

    Remove redundant diaginit.py

--- diaginit.py
+++
@@ -1,5 +0,0 @@
-print("\x1B[?1049h\x1B[2J\x1B[HDiagnostic Display for Pong")
-
-print("\x1B[2;H \x1B[4m                                                               \x1B[m")#Prints top bar of table
-print("\x1B[3;H| Player | ADC Val. | Button 1 | Button 2 | Bat Pos. | SuperBat |")
-print("\x1B[6;H ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯")#Prints bottom bar of table
